5cccde7e898bcec2d3e89620e49ef0462788d57f
Gillian Fries <fries@epsitec.ch>
2020-09-15 14:32:06 +0200

    Add config file in goblin-overwatch

---
+++ config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = [
+  {
+    type: 'input',
+    name: 'channels',
+    message: 'list of available channels',
+    default: {discord: [], mail: [], whatsApp: []},
+  },
+];
